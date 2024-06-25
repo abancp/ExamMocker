d4e788664196359172a74de5face60a2412ad3ee
Aban Muhammed C P <abancpchengani@gmail.comm>
2024-06-25 18:10:21 +0530

    more exam ui improovement

--- client/app/JEE/exam/page.jsx
+++ client/app/JEE/exam/page.jsx
@@ -1,3 +1,5 @@
+import NotAnswered from '../../../components/NotAnswered'
+
 const JeeExam = () => {
 
   return (
@@ -17,24 +19,38 @@ const JeeExam = () => {
           <div className="bg-blue-700 p-1 px-2 h-fit">Mathematics</div>
         </div>
       </div>
-      <div className="px-10">
+      <div className="px-10 flex gap-3">
         <div className="w-[70%]">
           <div className="flex px-3 py-1 mt-2 justify-between border-b-[1px] border-black items-center">
             <h1 className="text-lg font-semibold">Question 1:</h1>
             <div className="p-2 py-1 text-white rounded-full bg-blue-700"><h1 className="text-2xl  font-bold">↓</h1></div>
           </div>
-          <div className="h-[23rem] overflow-scroll">
+          <div className="h-[23rem] border-b border-black overflow-y-scroll">
             <h1 className="p-3 text-xl font-sans font-semibold">How many trinagles in the latest model of trigmoetryc ration of pythagorous in the mirror of like as a mirror in the hollowgram with my predictions that avoid How many trinagles in the latest model of trigmoetryc ration of pythagorous in the mirror of like as a mirror in the hollowgram with my predictions that avoid How many trinagles in the latest model of trigmoetryc ration of pythagorous in the mirror of like as a mirror in the hollowgram with my predictions that avoid </h1>
             <div className="flex flex-col gap-3 p-3 ">
-              <label htmlFor="mcq_option_1" className="flex gap-1"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_1" /><h1 className="text-xl font-semibold"> (1) <span className="ml-5">3</span></h1></label>
-              <label htmlFor="mcq_option_2" className="flex gap-2"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_2" /><h1 className="text-xl font-semibold"> (2) <span className="ml-5">1</span></h1></label>
+              <label htmlFor="mcq_option_1" className="flex gap-3"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_1" /><h1 className="text-xl font-semibold"> (1) <span className="ml-5">3</span></h1></label>
+              <label htmlFor="mcq_option_2" className="flex gap-3"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_2" /><h1 className="text-xl font-semibold"> (2) <span className="ml-5">1</span></h1></label>
               <label htmlFor="mcq_option_3" className="flex gap-3"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_3" /><h1 className="text-xl font-semibold"> (3) <span className="ml-5">2</span></h1></label>
-              <label htmlFor="mcq_option_4" className="flex gap-4"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_4" /><h1 className="text-xl font-semibold"> (4) <span className="ml-5">6</span></h1></label>
-            </div>
-            <div className="flex flex-col gap-3 p-3 ">
-
+              <label htmlFor="mcq_option_4" className="flex gap-3"><input className="cursor-pointer" type="radio" name="mcq_option" id="mcq_option_4" /><h1 className="text-xl font-semibold"> (4) <span className="ml-5">6</span></h1></label>
             </div>
           </div>
+          
+          <div className="flex mt-5 gap-3">
+            <button className="bg-green-600  px-2 py-1 text-lg font-semibold text-white">Save & Next</button>
+            <button className="bg-orange-500  px-2 py-1 text-lg font-semibold text-white">Save & Mark For Review</button>
+            <button className="border border-black  px-2 py-1 text-lg font-semibold text-black">Clear Response</button>
+            <button className="bg-orange-500  px-2 py-1 text-lg font-semibold text-white">Mark For Review & Next</button>
+          </div>
+          <div className="flex mt-5 p-3 bg-gray-200 justify-between font-semibold">
+            <div className="flex gap-2">
+              <button className="px-2 py-1 border border-black text-black">BACK</button>
+              <button className="px-2 py-1 border border-black text-black">NEXT</button>
+            </div>
+            <button className="px-2 py-1 bg-green-600 text-white font-semibold">SUBMIT</button>
+          </div>
+        </div>
+        <div>
+          {/* <NotAnswered/> */}
         </div>
       </div>
     </div>
---
+++ client/components/NotAnswered.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotAnswered() {
+    return (
+        <div class="sloped-di bg-orange-600">
+            1
+        </div>
+    )
+}
+
+export default NotAnswered
