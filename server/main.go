efb75a6046048f88b7989444662907b36ca73454
Aban Muhammed C P <abancpchengani@gmail.comm>
2024-06-29 10:10:36 +0530

    used gin

--- server/main.go
+++ server/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main()  {
-	
+import (
+	"server/auth"
+	"server/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//connect to mongodb
+	config.ConnectDb()
+
+	// Initialize a Gin router
+	r := gin.Default()
+
+	//routes
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	auth.Auth(r)
+
+	// Start the server on port 8080
+	r.Run(":8080")
 }
