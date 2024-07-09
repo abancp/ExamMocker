c55c48a58c730483e1f46fc30f0e0c3098d113d5
Aban Muhammed C P <abancpchengani@gmail.com>
2024-07-09 07:06:29 +0530

    useing cors middleware

--- server/main.go
+++ server/main.go
@@ -5,7 +5,9 @@ import (
 	"server/auth"
 	"server/config"
 	"server/exam"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -23,6 +25,15 @@ func main() {
 
 	// Initialize a Gin router
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"https://exam-mocker.vercel.app","http://localhost:3000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
 
 	//routes
 	r.GET("/hello", func(c *gin.Context) {
