ee92229a870db1b97e37baaf1c4e07e89b05052c
Aban Muhammed C P <abancpchengani@gmail.com>
2024-07-13 16:55:04 +0530

    admin question page and question-set page for jee wxam

--- client/app/admin/add/jee/question-set/page.jsx
+++ client/app/admin/add/jee/question-set/page.jsx
@@ -2,16 +2,27 @@
 
 import { useEffect, useMemo, useState } from 'react'
 import Header from '../../../../../components/Admin/Header'
+import axios from 'axios'
+import SERVER_URL from '../../../../../config/serverUrl'
+import Link from 'next/link'
+
 
 function page() {
   //M:Mathematics | P:Physics | C:Chemistry
   const [subject, setSubject] = useState("M")
   //Q:Question  | A:Option A  |  B:Option B   |  C:Option C   |  D:Option D
-  const [questionType,setQuestionType] = useState("Q")
+  const [questionType, setQuestionType] = useState("Q")
   const [questionNumber, setQuestionNumber] = useState(1)
   const [lastIndexes, setLastIndexes] = useState({ M: 1, P: 1, C: 1 })
-  const [lastType,setLastType] = useState({})
+  const [lastType, setLastType] = useState({})
   const questionsIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    axios.get(SERVER_URL + "/admin/questions").then(({ data }) => {
+      setQuestions(data.questions)
+    })
+  }, [])
 
   const changeSubject = (e) => {
     lastIndexes[subject] = questionNumber
@@ -19,23 +30,27 @@ function page() {
     setQuestionNumber(lastIndexes[e.target.id])
   }
 
-  const changeQuestionType = (e) =>{
+  const changeQuestionType = (e) => {
     setQuestionType(e.target.id)
   }
 
-  const changeQuestionNumber = (e) => { 
-    lastType[questionNumber+subject] = questionType
+  const changeQuestionNumber = (e) => {
+    lastType[questionNumber + subject] = questionType
     setQuestionNumber(Number(e.target.id))
-    if(lastType[e.target.id+subject]){
-      setQuestionType(lastType[e.target.id+subject])
-    }else{
+    if (lastType[e.target.id + subject]) {
+      setQuestionType(lastType[e.target.id + subject])
+    } else {
       setQuestionType("Q")
     }
   }
 
+  const setQuestion = () =>{
+
+  }
+
   return (
 
-    <div className='h-screen flex flex-col pb-7 gap-2 pt-[2.7rem] px-7 w-full'>
+    <div className='min-h-screen max-h-fit flex flex-col pb-3 gap-2 pt-[2.7rem] px-3 w-full'>
       <Header />
       <div className='w-full  items-center flex justify-around '>
         <div onClick={changeSubject} id='M' className={`${subject === "M" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-300 px-3 pb-2`} >Mathematics</div>
@@ -49,9 +64,19 @@ function page() {
           ))
         }
       </div>
-      
-      <div className='border bg-white border-[#259ac4] h-full w-full'>
-        <textarea className='text-xl font-semibold text-black focus:outline-none w-full h-full'  name="" id=""></textarea>
+
+      <div className='border p-2 border-[#259ac4] flex flex-col gap-2 h-full w-full'>
+        <div className='border border-[#40c425] flex justify-center items-center border-dashed h-20 w-full mb-3'>
+          <h1 className="text-3xl font-semibold text-[#259ac481]">Question Is Here</h1>
+        </div>
+        {
+          questions.map((question) => (
+            <div href={'/admin/question/' + question._id} className='border-[#259ac4] w-full flex justify-between  p-2 border'>
+              <div className="whitespace-nowrap overflow-hidden w-full text-ellipsis">{question.question}</div>
+              <div onClick={()=>{setQuestion(question)}} className=' flex items-center '><Link href={'/admin/question/' + question._id} className='  px-1 py-[.10rem] bg-[#35353f58] '>Open</Link></div>
+            </div>
+          ))
+        }
       </div>
     </div>
 
---
+++ client/app/admin/question/[id]/page.jsx
@@ -0,0 +1,35 @@
+'use client'
+
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import SERVER_URL from '../../../../config/serverUrl'
+import { useParams } from 'next/navigation'
+
+function page() {
+  const { id } = useParams()
+  const [question, setQuestion] = useState({})
+  useEffect(() => {
+    axios.get(`${SERVER_URL}/admin/question/${id}`).then(({ data }) => {
+      setQuestion(data.question)
+      console.log(data.question)
+    })
+  }, [])
+  return (
+    <div className='w-full gap-2 flex px-2 flex-col  min-h-screen max-h-fit'>
+      <div className='w-full py-2 pb-0'>
+        <div className='border border-[#259ac4] bg-[#0e0e1f] text-[#ffffff] flex flex-col gap-4  font-light w-full p-2 py-3 '>
+          <h1 className=' text-center w-full text-xl font-semibold'>Question</h1>
+          <div className='flex gap-3'><div className='text-lg font-medium'>Q:</div><div className='font-light'>{question?.question}</div></div>
+          {question?.options?.map((option, i) => (
+          <div className=' flex gap-3 w-full text-[#ffffff]'><div className='h-full text-lg font-semibold'>{i == 0 ? "A" : i == 1 ? "B" : i == 2 ? "C" : i == 3 && "D"}:</div><h1 className='font-light'>{option}</h1></div>
+        ))}
+        </div>
+      </div>
+      {/* <div className='w-full   py-3 flex flex-col gap-3 font-light'> */}
+        
+      {/* </div> */}
+    </div>
+  )
+}
+
+export default page
