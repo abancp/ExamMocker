52dc53d936e2cd8162352f63e67f064c45771a7b
Aban Muhammed C P <abancpchengani@gmail.com>
2024-07-13 19:26:59 +0530

    api integration completed for login

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,13 +1,31 @@
+"use client"
+import axios from "axios"
 import Header from "../../components/Header"
 import Link from "next/link"
+import SERVER_URL from "../../config/serverUrl"
+
 
 function Login() {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const credentials = {
+            email:e.target[0].value,
+            password:e.target[1].value
+        }
+        login(credentials)
+    }
+
+    const login = async (credentials) => {
+       const result = await axios.post(SERVER_URL+"/login",credentials,{withCredentials:true})
+       console.log(result)
+    }
+
     return (
         <div className="w-full h-screen flex pl-40 p-10 items-center ">
             <Header/>
             <div className="w-[450px] py-3 flex flex-col justify-around items-center h-[560px] border border-[#2c230f] rounded-lg ">
                 <h1 className=" text-3xl font-thin font-sans">ExamMocker Login</h1>
-                <form className="flex flex-col gap-5" action="">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-5" action="">
                     <input placeholder="Email" className=" p-1 rounded-lg text-lg border border-[#2c230f] focus:outline-none bg-transparent " type="text" />
                     <input placeholder="Password" className=" p-1 rounded-lg text-lg border border-[#2c230f] focus:outline-none bg-transparent" type="password" />
                     <input placeholder="Submit" className=" p-1 rounded-lg text-lg border border-[#2c230f] focus:outline-none outline-none  hover:bg-[#8067335f] duration-300 cursor-pointer bg-[#2c230f5f]" type="submit" />
