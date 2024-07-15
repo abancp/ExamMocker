6482a2db3a23d5ed2a8d8752e25e3ec1eec652d9
Aban Muhammed C P <abancpchengani@gmail.com>
2024-07-15 20:34:36 +0530

    mathjax support for some pages

--- client/app/admin/add/jee/question-set/page.jsx
+++ client/app/admin/add/jee/question-set/page.jsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import SERVER_URL from '../../../../../config/serverUrl'
 import Link from 'next/link'
 import useDebounce from "../../../../../hooks/useDebounce"
+import { MathJax, MathJaxContext } from 'better-react-mathjax'
 
 function page() {
   //M:Mathematics | P:Physics | C:Chemistry
@@ -21,7 +22,7 @@ function page() {
   const [exam] = useState({ mathematics: {}, physics: {}, chemistry: {} })
   const [topics, setTopics] = useState({ mathematics: [], physics: [], chemistry: [] })
   const [topic, setTopic] = useState("")
-  const [searchQuery,setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
 
   const debounce = useDebounce()
 
@@ -78,78 +79,79 @@ function page() {
     }
   }
 
-  const searchQuestions = async (search,subjectForSearch,topicForSearch) =>{
+  const searchQuestions = async (search, subjectForSearch, topicForSearch) => {
     console.log(`searching for subject : ${subjectForSearch} , topic : ${topicForSearch} , query : ${search}`)
-      axios.get(`${SERVER_URL}/admin/questions?subject=${subjectForSearch}&topic=${topicForSearch}&search=${search}`).then(({ data }) => {
-        setQuestions(data.questions)
-      })
+    axios.get(`${SERVER_URL}/admin/questions?subject=${subjectForSearch}&topic=${topicForSearch}&search=${search}`).then(({ data }) => {
+      setQuestions(data.questions)
+    })
   }
-  
-  const debounceGetQuestions = useCallback(debounce(searchQuestions,200),[])
+
+  const debounceGetQuestions = useCallback(debounce(searchQuestions, 200), [])
 
   return (
-
-    <div className='min-h-screen max-h-fit flex flex-col pb-3 gap-2 pt-[2.7rem] px-3 w-full'>
-      <Header />
-      <div className='w-full  items-center flex justify-around '>
-        <div onClick={changeSubject} id='mathematics' className={`${subject === "mathematics" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`} >Mathematics</div>
-        <div onClick={changeSubject} id='physics' className={`${subject === "physics" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`}>Physics</div>
-        <div onClick={changeSubject} id='chemistry' className={`${subject === "chemistry" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`}>Chemistry</div>
-      </div>
-      <div className='flex  gap-[.7rem]  w-full justify-center items-center '>
-        {
-          questionsIndex.map((questionIndex) => (
-            <div onClick={changeQuestionNumber} key={questionIndex} id={questionIndex} className={` ${questionNumber === questionIndex && "bg-[#21213b] text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] duration-300  flex justify-center items-center w-9 h-9 border border-[#259ac4]`}>{questionIndex}</div>
-          ))
-        }
-      </div>
-
-      <div className='w-full border border-[#259ac4] p-2 flex justify-around items-center'>
-
-        <div className='flex items-center w-60 border border-[#259ac4]'>
-          <input id='search' onChange={(e) => { setSearchQuery(e.target.value);debounceGetQuestions(e.target.value,subject,topic) }} className='px-1 w-full bg-transparent text-white font-light text-lg focus:outline-none' placeholder='search here' type="text" />
-          <div onClick={()=>{searchQuestions(searchQuery,subject,topic)}} className='border-l p-2 border-[#259ac4]'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi cursor-pointer duration-300 hover:text-[#259ac4] bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
-          </div>
+    <MathJaxContext>
+      <div className='min-h-screen max-h-fit flex flex-col pb-3 gap-2 pt-[2.7rem] px-3 w-full'>
+        <Header />
+        <div className='w-full  items-center flex justify-around '>
+          <div onClick={changeSubject} id='mathematics' className={`${subject === "mathematics" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`} >Mathematics</div>
+          <div onClick={changeSubject} id='physics' className={`${subject === "physics" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`}>Physics</div>
+          <div onClick={changeSubject} id='chemistry' className={`${subject === "chemistry" && "border-b text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] border-[#259ac4] pt-2 duration-200 px-3 pb-2`}>Chemistry</div>
+        </div>
+        <div className='flex  gap-[.7rem]  w-full justify-center items-center '>
+          {
+            questionsIndex.map((questionIndex) => (
+              <div onClick={changeQuestionNumber} key={questionIndex} id={questionIndex} className={` ${questionNumber === questionIndex && "bg-[#21213b] text-[#259ac4]"} cursor-pointer hover:bg-[#21213b] duration-300  flex justify-center items-center w-9 h-9 border border-[#259ac4]`}>{questionIndex}</div>
+            ))
+          }
         </div>
 
-        <div className='flex w-60 border border-[#259ac4]'>
-          <select onChange={(e) => { setTopic(e.target.value) }} className='px-1  p-[.35rem] flex items-center justify-center w-60 text-lg  bg-transparent text-white font-light focus:outline-none'>
-            <option value={""} className='text-[#2b2b2b91]'>--select topic--</option>
-            {
-              topics[subject].map((topic) => (
-                <option className='text-black' value={topic}>{topic}</option>
-              ))
-            }
-          </select>
-        </div>
+        <div className='w-full border border-[#259ac4] p-2 flex justify-around items-center'>
 
-      </div>
-
-      <div className='border p-2 border-[#259ac4] flex flex-col gap-2 h-full w-full'>
-        <div className='border border-[#40c425] flex justify-center items-center border-dashed min-h-20 max-h-fit px-3 w-full'>
-          {currentQuestion ?
-            <div className='w-full py-2 pb-0'>
-              <h1 className=' text-center w-full text-xl font-semibold'>Question</h1>
-              <div className='flex gap-3'><div className='text-lg font-medium'>Q:</div><div className='font-light'>{currentQuestion?.question}</div></div>
-              {currentQuestion?.options?.map((option, i) => (
-                <div className=' flex gap-3 w-full text-[#ffffff]'><div className='h-full text-lg font-semibold'>{i == 0 ? "A" : i == 1 ? "B" : i == 2 ? "C" : i == 3 && "D"}:</div><h1 className='font-light'>{option}</h1></div>
-              ))}
-            </div> :
-            <h1 className="text-3xl font-semibold text-[#259ac481]">Question Is Here</h1>}
-        </div>
-        {
-          questions?.map((question) => (
-            <div href={'/admin/question/' + question._id} className='border-[#259ac4] w-full flex justify-between  p-2 border'>
-              <div onClick={() => { setCurrentQuestion(question) }} className="whitespace-nowrap overflow-hidden w-full text-ellipsis">{question.question}</div>
-              <div className=' flex items-center'><Link href={'/admin/question/' + question._id} className='  px-1 py-[.10rem] bg-[#35353f58] '>Open</Link></div>
+          <div className='flex items-center w-60 border border-[#259ac4]'>
+            <input id='search' onChange={(e) => { setSearchQuery(e.target.value); debounceGetQuestions(e.target.value, subject, topic) }} className='px-1 w-full bg-transparent text-white font-light text-lg focus:outline-none' placeholder='search here' type="text" />
+            <div onClick={() => { searchQuestions(searchQuery, subject, topic) }} className='border-l p-2 border-[#259ac4]'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi cursor-pointer duration-300 hover:text-[#259ac4] bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
             </div>
-          ))
-        }
+          </div>
+
+          <div className='flex w-60 border border-[#259ac4]'>
+            <select onChange={(e) => { setTopic(e.target.value) }} className='px-1  p-[.35rem] flex items-center justify-center w-60 text-lg  bg-transparent text-white font-light focus:outline-none'>
+              <option value={""} className='text-[#2b2b2b91]'>--select topic--</option>
+              {
+                topics[subject].map((topic) => (
+                  <option className='text-black' value={topic}>{topic}</option>
+                ))
+              }
+            </select>
+          </div>
+
+        </div>
+
+        <div className='border p-2 border-[#259ac4] flex flex-col gap-2 h-full w-full'>
+          <div className='border border-[#40c425] flex justify-center items-center border-dashed min-h-20 max-h-fit px-3 w-full'>
+            {currentQuestion ?
+              <div className='w-full py-2 pb-0'>
+                <h1 className=' text-center w-full text-xl font-semibold'>Question</h1>
+                <div className='flex gap-3'><div className='text-lg font-medium'>Q:</div><div className='font-light'><MathJax>{currentQuestion?.question}</MathJax></div></div>
+                {currentQuestion?.options?.map((option, i) => (
+                  <div className=' flex gap-3 w-full text-[#ffffff]'><div className='h-full text-lg font-semibold'>{i == 0 ? "A" : i == 1 ? "B" : i == 2 ? "C" : i == 3 && "D"}:</div><h1 className='font-light'><MathJax>{option}</MathJax></h1></div>
+                ))}
+              </div> :
+              <h1 className="text-3xl font-semibold text-[#259ac481]">Question Is Here</h1>}
+          </div>
+          {
+            questions?.map((question) => (
+              <div href={'/admin/question/' + question._id} className='border-[#259ac4] w-full flex justify-between  p-2 border'>
+                <div onClick={() => { setCurrentQuestion(question) }} className="whitespace-nowrap overflow-hidden w-full text-ellipsis"><MathJax>{question.question}</MathJax></div>
+                <div className=' flex items-center'><Link href={'/admin/question/' + question._id} className='  px-1 py-[.10rem] bg-[#35353f58] '>Open</Link></div>
+              </div>
+            ))
+          }
+        </div>
       </div>
-    </div>
+    </MathJaxContext>
 
   )
 }
