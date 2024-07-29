6d3c71e2f55ef1c951bbc910bf546f3da7e08e29
Aban Muhammed C P <abancpchengani@gmail.com>
2024-07-29 10:27:18 +0530

    exam adding api integration completed

--- client/app/admin/add/jee/exam/page.jsx
+++ client/app/admin/add/jee/exam/page.jsx
@@ -5,26 +5,30 @@ import Input from '../../../../../components/Admin/Input';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import SERVER_URL from "../../../../../config/serverUrl"
+import { toast } from 'sonner';
 
 function page() {
 
   const router = useRouter()
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault()
-    console.log({exam:e.target[0].value,date:e.target[1].value,examId:e.target[2].value});
-    axios.post(SERVER_URL+'/admin/exam',{exam:e.target[0].value,date:e.target[1].value,examId:e.target[2].value})
-    router.push("/admin/add/jee/question-set")
+    console.log({ exam: e.target[0].value, date: e.target[1].value, examId: e.target[2].value });
+    const { data } = await axios.post(SERVER_URL + '/admin/exam', { exam: e.target[0].value, date: e.target[1].value, examId: e.target[2].value })
+    if(data.success){
+      router.push("/admin/add/jee/question-set/"+data.id)
+    }else{
+      toast.error(data.message || "something went wrong!")
+    }
   }
 
   return (
     <div className='w-full flex flex-col pt-16 items-center justify-center h-screen '>
-      <Header/>
+      <Header />
       <h1 className="text-2xl font-light">Add Jee Mock Test Here</h1>
-      <form  className='flex mt-7 flex-col gap-2 justify-center' onSubmit={submitHandler}>
-        <label> <span className='inline-block text-left w-28'>Exam</span><Input type="text" value="JEE" disabled/> </label>        
-        <label> <span className='inline-block text-left w-28'>Date & Time</span><Input type="text"/> </label>        
-        <label> <span className='inline-block text-left w-28'>Exam ID</span><Input type="text" disabled value="JEE2024MorningSection"/> </label>  
-        <input  type="submit" value="Add Questions" className='border p-1 border-[#259ac4] rounded-md bg-[#21213b] duration-300 cursor-pointer hover:bg-[#323262]' />
+      <form className='flex mt-7 flex-col gap-2 justify-center' onSubmit={submitHandler}>
+        <label> <span className='inline-block text-left w-28'>Exam</span><Input type="text" value="JEE" disabled /> </label>
+        <label> <span className='inline-block text-left w-28'>Date & Time</span><Input type="text" /> </label>
+        <input type="submit" value="Add Questions" className='border p-1 border-[#259ac4] rounded-md bg-[#21213b] duration-300 cursor-pointer hover:bg-[#323262]' />
       </form>
     </div>
   )
