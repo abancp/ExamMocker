0019201a62c5aa32932a3f0835ae9e415465648b
Aban Muhammed C P <abancpchengani@gmail.com>
2024-08-03 10:18:51 +0530

    defalut browser date picker for add exam

--- client/app/admin/add/jee/exam/page.jsx
+++ client/app/admin/add/jee/exam/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React,{useState} from 'react'
 import Header from '../../../../../components/Admin/Header'
 import Input from '../../../../../components/Admin/Input';
 import { useRouter } from 'next/navigation';
@@ -7,13 +7,14 @@ import axios from 'axios';
 import SERVER_URL from "../../../../../config/serverUrl"
 import { toast } from 'sonner';
 
+
 function page() {
 
   const router = useRouter()
+
   const submitHandler = async (e) => {
     e.preventDefault()
-    console.log({ exam: e.target[0].value, date: e.target[1].value, examId: e.target[2].value });
-    const { data } = await axios.post(SERVER_URL + '/admin/exam', { exam: e.target[0].value, date: e.target[1].value, examId: e.target[2].value })
+    const { data } = await axios.post(SERVER_URL + '/admin/exam', { exam: e.target[0].value, date: e.target[1].value})
     if(data.success){
       router.push("/admin/add/jee/question-set/"+data.id)
     }else{
@@ -21,13 +22,15 @@ function page() {
     }
   }
 
+
   return (
     <div className='w-full flex flex-col pt-16 items-center justify-center h-screen '>
       <Header />
       <h1 className="text-2xl font-light">Add Jee Mock Test Here</h1>
       <form className='flex mt-7 flex-col gap-2 justify-center' onSubmit={submitHandler}>
         <label> <span className='inline-block text-left w-28'>Exam</span><Input type="text" value="JEE" disabled /> </label>
-        <label> <span className='inline-block text-left w-28'>Date & Time</span><Input type="text" /> </label>
+        <label> <span className='inline-block text-left w-28'>Date & Time</span><Input type="datetime-local" /></label>
+        
         <input type="submit" value="Add Questions" className='border p-1 border-[#259ac4] rounded-md bg-[#21213b] duration-300 cursor-pointer hover:bg-[#323262]' />
       </form>
     </div>
