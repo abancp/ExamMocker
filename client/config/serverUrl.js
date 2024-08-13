e4f1c66ef9d3ad6da98d2d1a78360924ea3eca73
Aban Muhammed C P <abancpchengani@gmail.com>
2024-08-13 21:17:36 +0530

    server chenaged

--- client/config/serverUrl.js
+++ client/config/serverUrl.js
@@ -1,2 +1,3 @@
-const SERVER_URL = process.env.NODE_ENV == "production" ? "https://exammocker.onrender.com" : "http://localhost:8080"
+const SERVER_URL = process.env.NODE_ENV == "production" ? "https://aggressive-verla-exam-mocker-9227ebd4.koyeb.app" : "http://localhost:8080"
+//const SERVER_URL = process.env.NODE_ENV == "production" ? "https://exammocker.onrender.com" : "http://localhost:8080"
 export default SERVER_URL
