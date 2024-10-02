02d794d96c0074d2986d2c2e8cb213c6f9d1fbd1
Aban Muhammed C P <abancpchengani@gmail.com>
2024-10-02 07:50:57 +0530

    quck change in store when user loged in

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,65 +1,104 @@
-"use client"
-import { useState } from "react"
-import axios from "axios"
-import Header from "../../components/Header"
-import Link from "next/link"
-import SERVER_URL from "../../config/serverUrl"
-import { useRouter } from "next/navigation"
-import { toast } from "sonner"
-
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import Header from "../../components/Header";
+import Link from "next/link";
+import SERVER_URL from "../../config/serverUrl";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import useStore from "../../store/store";
 
 function Login() {
+  const [errMsg, setErrMsg] = useState(null);
 
-    const [errMsg, setErrMsg] = useState(null)
+  const router = useRouter();
 
-    const router = useRouter()
+  const [setIsLogin, setUsername, setAdmin] = useStore((state) => [
+    state.setIsLogin,
+    state.setUsername,
+    state.setAdmin,
+  ]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setErrMsg(null)
-        const credentials = {
-            email: e.target[0].value,
-            password: e.target[1].value
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrMsg(null);
+    const credentials = {
+      email: e.target[0].value,
+      password: e.target[1].value,
+    };
+    login(credentials);
+  };
+
+  const login = (credentials) => {
+    axios
+      .post(SERVER_URL + "/login", credentials, { withCredentials: true })
+      .then(({ data }) => {
+        if (data.success) {
+          console.log("Login Successful");
+          toast.success("Login successful!");
+          axios
+            .get(SERVER_URL + "/validate-token", { withCredentials: true })
+            .then(({ data }) => {
+              if (data.success && data.claims.username) {
+                setIsLogin(true);
+              }
+              setUsername(data.claims.username);
+              setAdmin(data.claims.admin);
+            });
+          router.push("/");
+        } else {
+          toast.error(data.error || "something went wrong!");
         }
-        login(credentials)
-    }
+      })
+      .catch((data) => {
+        if (data?.response?.status == 401) {
+          setErrMsg(data.response.data.error);
+        } else {
+          toast.error(data.response.data.error || "something went wrong!");
+        }
+      });
+  };
 
-    const login = (credentials) => {
-        axios.post(SERVER_URL + "/login", credentials, { withCredentials: true }).then(({ data }) => {
-            if (data.success) {
-                console.log("Login Successful")
-                toast.success("Login successful!")
-                router.push("/")
-            } else {
-                toast.error(data.error || "something went wrong!")
-            }
-        }).catch((data) => {
-            if (data?.response?.status == 401) {
-                setErrMsg(data.response.data.error)
-            } else {
-                toast.error(data.response.data.error || "something went wrong!")
-            }
-        })
-
-    }
-
-    return (
-        <div className="w-full h-screen justify-center flex pt-40 md:pt-24 p-10 items-center ">
-            <Header />
-            <div className="w-[450px] md:w-[1000px]  md:flex-row flex-col flex gap-20 py-20 justify-between items-center md:h-[400px] h-[560px] border border-primary rounded-lg ">
-                <div className="md:w-1/2 w-full h-1  flex flex-col items-center justify-center">
-                    <h1 className=" text-3xl font-bold font-sans">ExamMocker Login</h1>
-                </div>
-
-                <form onSubmit={handleSubmit} className="md:w-1/2 w-full px-8 flex flex-col items-center justify-center gap-5" action="">
-                    <p className="text-red-600 ">{errMsg}</p>
-                    <input placeholder="Email" className=" p-1 rounded-lg text-lg border border-primary focus:outline-none bg-transparent w-full" type="text" />
-                    <input placeholder="Password" className=" p-1 rounded-lg text-lg border border-primary focus:outline-none bg-transparent w-full" type="password" />
-                    <input value="LOGIN" className="font-bold text-xl p-1 rounded-lg text-black bg-[#259ac4] hover:bg-[#65c3e5] focus:outline-none outline-none w-full  duration-300 cursor-pointer" type="submit" />
-                <h5 >not have a account <Link className="text-blue-500" href={'/signup'}> signup here </Link></h5>
-                </form>
-            </div>
+  return (
+    <div className="w-full h-screen justify-center flex pt-40 md:pt-24 p-10 items-center ">
+      <Header />
+      <div className="w-[450px] md:w-[1000px]  md:flex-row flex-col flex gap-20 py-20 justify-between items-center md:h-[400px] h-[560px] border border-primary rounded-lg ">
+        <div className="md:w-1/2 w-full gap-3  flex flex-col items-center justify-center">
+          <h1 className=" text-3xl font-bold font-sans">ExamMocker Login</h1>
+          <h5 className="md:block hidden">not have a account <Link className="text-blue-500" href={'/signup'}> signup here </Link></h5>
         </div>
-    )
+
+        <form
+          onSubmit={handleSubmit}
+          className="md:w-1/2 w-full px-8 flex flex-col items-center justify-center gap-5"
+          action=""
+        >
+          <p className="text-red-600 ">{errMsg}</p>
+          <input
+            placeholder="Email"
+            className=" p-1 rounded-lg text-lg border border-primary focus:outline-none bg-transparent w-full"
+            type="text"
+          />
+          <input
+            placeholder="Password"
+            className=" p-1 rounded-lg text-lg border border-primary focus:outline-none bg-transparent w-full"
+            type="password"
+          />
+          <input
+            value="LOGIN"
+            className="font-bold text-xl p-1 rounded-lg text-black bg-[#259ac4] hover:bg-[#65c3e5] focus:outline-none outline-none w-full  duration-300 cursor-pointer"
+            type="submit"
+          />
+          <h5 className="md:hidden">
+            not have a account{" "}
+            <Link className="text-blue-500" href={"/signup"}>
+              {" "}
+              signup here{" "}
+            </Link>
+          </h5>
+        </form>
+      </div>
+    </div>
+  );
 }
-export default Login
+export default Login;
